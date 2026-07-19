9199b72cf9acece704579eb1818c2763cb69227b
dawaapharmacy29-create <dawaapharmacy29@gmail.com>
2026-07-19 23:11:09 +0300

    show customer service Excel export and log explanation

--- src/pages/SmartCustomerService.tsx
+++ src/pages/SmartCustomerService.tsx
@@ -1,9 +1,11 @@
+import CustomerServiceDataTools from '@/components/customerService/CustomerServiceDataTools';
 import CustomerServiceOperationsPanel from '@/components/customerService/CustomerServiceOperationsPanel';
 import UnifiedCustomerServiceWorkspace from '@/components/customerService/UnifiedCustomerServiceWorkspace';
 
 export default function SmartCustomerService() {
   return (
     <div className="customer-service-page" dir="rtl">
+      <CustomerServiceDataTools />
       <CustomerServiceOperationsPanel />
       <UnifiedCustomerServiceWorkspace />
     </div>
